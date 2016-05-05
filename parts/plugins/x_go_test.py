e0a4a9254b8f854c38e69fa9bdec24cfbd993320
Paul Larson <paul.larson@canonical.com>
2016-05-04 20:38:28 -0500

    Snap for building the snappy upstream tests

---
+++ parts/plugins/x_go_test.py
@@ -0,0 +1,30 @@
+import os
+from snapcraft.plugins.go import GoPlugin
+
+class GoTestPlugin(GoPlugin):
+
+    """
+    def _remote_build(self):
+        pass
+    def clean_build(self):
+        super().clean_build()
+    """
+    def build(self):
+        install_bin_path = os.path.join(self.installdir, 'bin')
+        os.makedirs(install_bin_path, exist_ok=True)
+        self._run(['go', 'test', '-c', 'snappy/integration-tests/tests', '-o', os.path.join(install_bin_path, 'snappy-tests')])
+        return
+"""
+    def build(self):
+        super().build()
+        if self.options.source is not None:
+            self._local_build()
+        self._remote_build()
+
+        install_bin_path = os.path.join(self.installdir, 'bin')
+        os.makedirs(install_bin_path, exist_ok=True)
+        os.makedirs(self._gopath_bin, exist_ok=True)
+        for binary in os.listdir(os.path.join(self._gopath_bin)):
+            binary_path = os.path.join(self._gopath_bin, binary)
+            shutil.copy2(binary_path, install_bin_path)
+"""
